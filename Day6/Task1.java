aeef1855ee737e102242ddc5d02b35189e5274a0
harini1165 <cnharini195@gmail.com>
2025-06-28 21:57:35 +0530

    Update Task1.java

--- Day6/Task1.java
+++ Day6/Task1.java
@@ -5,7 +5,7 @@ class NumberThread extends Thread {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Number: " + i);
             try {
-                Thread.sleep(500); // Sleep for 500 milliseconds
+                Thread.sleep(500); //sleep
             } catch (InterruptedException e) {
                 System.out.println("Thread interrupted.");
             }
